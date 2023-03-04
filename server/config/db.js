1a0e1de9abc40a2542b848b2d0bd906bd0cc347f
MoneyRaheja08 <122489723+MoneyRaheja08@users.noreply.github.com>
2023-03-03 19:45:59 -0500

    first

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+//db.js Money Raheja 301276615 03/03/2023 Mid-Term Test
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://moneyraheja:4vJQzuU9779E5mZ9@cluster0.ajkufxz.mongodb.net/?retryWrites=true&w=majority"
+};
---
+++ server/models/books.js
@@ -0,0 +1,16 @@
+//books.js Money Raheja 301276615 03/03/2023 Mid-Term Test
+let mongoose = require('mongoose');
+
+// create a model class
+let Book = mongoose.Schema({
+    Title: String,
+    Description: String,
+    Price: Number,
+    Author: String,
+    Genre: String
+},
+{
+  collection: "books"
+});
+
+module.exports = mongoose.model('Book', Book);
